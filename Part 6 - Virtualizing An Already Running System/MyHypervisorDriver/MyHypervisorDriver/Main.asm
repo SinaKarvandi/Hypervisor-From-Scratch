a74c188a0409f3d7a1839e0d1ea6e575d4c18692
Sina Karvandi <SinaKarvandi@users.noreply.github.com>
2019-07-08 00:29:45 +0430

    Add files via upload

---
+++ Part 6 - Virtualizing An Already Running System/MyHypervisorDriver/MyHypervisorDriver/Main.asm
@@ -0,0 +1,215 @@
+PUBLIC Enable_VMX_Operation
+PUBLIC Breakpoint
+PUBLIC STI_Instruction
+PUBLIC CLI_Instruction
+PUBLIC INVEPT_Instruction
+PUBLIC GetCs
+PUBLIC GetDs
+PUBLIC GetEs
+PUBLIC GetSs
+PUBLIC GetFs
+PUBLIC GetGs
+PUBLIC GetLdtr
+PUBLIC GetTr
+PUBLIC Get_GDT_Base
+PUBLIC Get_IDT_Base
+PUBLIC Get_GDT_Limit
+PUBLIC Get_IDT_Limit
+PUBLIC Get_RFLAGS
+PUBLIC Restore_To_VMXOFF_State
+PUBLIC Save_VMXOFF_State
+PUBLIC MSRRead
+PUBLIC MSRWrite
+
+EXTERN g_StackPointerForReturning:QWORD
+EXTERN g_BasePointerForReturning:QWORD
+
+
+.code _text
+
+
+;------------------------------------------------------------------------
+    VMX_ERROR_CODE_SUCCESS              = 0
+    VMX_ERROR_CODE_FAILED_WITH_STATUS   = 1
+    VMX_ERROR_CODE_FAILED               = 2
+;------------------------------------------------------------------------
+
+Enable_VMX_Operation PROC PUBLIC
+push rax			; Save the state
+
+xor rax,rax			; Clear the RAX
+mov rax,cr4
+or rax,02000h		; Set the 14th bit
+mov cr4,rax
+
+pop rax				; Restore the state
+ret
+Enable_VMX_Operation ENDP
+
+;------------------------------------------------------------------------
+     
+Breakpoint PROC PUBLIC
+int 3
+ret
+Breakpoint ENDP 
+
+;------------------------------------------------------------------------
+
+STI_Instruction PROC PUBLIC
+STI
+ret
+STI_Instruction ENDP 
+
+;------------------------------------------------------------------------
+
+CLI_Instruction PROC PUBLIC
+CLI
+ret
+CLI_Instruction ENDP 
+;------------------------------------------------------------------------
+
+Restore_To_VMXOFF_State PROC PUBLIC
+
+VMXOFF  ; turn it off before existing
+
+MOV rsp, g_StackPointerForReturning
+MOV rbp, g_BasePointerForReturning
+
+; make rsp point to a correct return point
+ADD rsp,8
+
+; return True
+xor rax,rax
+mov rax,1
+
+; return section
+
+mov     rbx, [rsp+28h+8h]
+mov     rsi, [rsp+28h+10h]
+add     rsp, 020h
+pop     rdi
+
+ret
+
+Restore_To_VMXOFF_State ENDP 
+
+;------------------------------------------------------------------------
+
+Save_VMXOFF_State PROC PUBLIC
+MOV g_StackPointerForReturning,rsp
+MOV g_BasePointerForReturning,rbp
+ret
+
+Save_VMXOFF_State ENDP 
+
+;------------------------------------------------------------------------
+INVEPT_Instruction PROC PUBLIC
+        invept  rcx, oword ptr [rdx]
+        jz @jz
+        jc @jc
+        xor     rax, rax
+        ret
+
+@jz:    mov     rax, VMX_ERROR_CODE_FAILED_WITH_STATUS
+        ret
+
+@jc:    mov     rax, VMX_ERROR_CODE_FAILED
+        ret
+INVEPT_Instruction ENDP
+
+;------------------------------------------------------------------------
+Get_GDT_Base PROC
+	LOCAL	gdtr[10]:BYTE
+	sgdt	gdtr
+	mov		rax, QWORD PTR gdtr[2]
+	ret
+Get_GDT_Base ENDP
+;------------------------------------------------------------------------
+GetCs PROC
+	mov		rax, cs
+	ret
+GetCs ENDP
+;------------------------------------------------------------------------
+GetDs PROC
+	mov		rax, ds
+	ret
+GetDs ENDP
+;------------------------------------------------------------------------
+GetEs PROC
+	mov		rax, es
+	ret
+GetEs ENDP
+;------------------------------------------------------------------------
+GetSs PROC
+	mov		rax, ss
+	ret
+GetSs ENDP
+;------------------------------------------------------------------------
+GetFs PROC
+	mov		rax, fs
+	ret
+GetFs ENDP
+;------------------------------------------------------------------------
+GetGs PROC
+	mov		rax, gs
+	ret
+GetGs ENDP
+;------------------------------------------------------------------------
+GetLdtr PROC
+	sldt	rax
+	ret
+GetLdtr ENDP
+;------------------------------------------------------------------------
+GetTr PROC
+	str	rax
+	ret
+GetTr ENDP
+;------------------------------------------------------------------------
+Get_IDT_Base PROC
+	LOCAL	idtr[10]:BYTE
+	
+	sidt	idtr
+	mov		rax, QWORD PTR idtr[2]
+	ret
+Get_IDT_Base ENDP
+;------------------------------------------------------------------------
+
+Get_GDT_Limit PROC
+	LOCAL	gdtr[10]:BYTE
+
+	sgdt	gdtr
+	mov		ax, WORD PTR gdtr[0]
+	ret
+Get_GDT_Limit ENDP
+
+;------------------------------------------------------------------------
+Get_IDT_Limit PROC
+	LOCAL	idtr[10]:BYTE
+	
+	sidt	idtr
+	mov		ax, WORD PTR idtr[0]
+	ret
+Get_IDT_Limit ENDP
+;------------------------------------------------------------------------
+Get_RFLAGS PROC
+	pushfq
+	pop		rax
+	ret
+Get_RFLAGS ENDP
+;------------------------------------------------------------------------
+MSRRead PROC
+	rdmsr				; MSR[ecx] --> edx:eax
+	shl		rdx, 32
+	or		rax, rdx
+	ret
+MSRRead ENDP
+;------------------------------------------------------------------------
+MSRWrite PROC
+	mov		rax, rdx
+	shr		rdx, 32
+	wrmsr
+	ret
+MSRWrite ENDP
+;------------------------------------------------------------------------
+
+END                                                                                                                                                                                                                   
---
+++ Part 6 - Virtualizing An Already Running System/MyHypervisorDriver/MyHypervisorDriver/Memory.c
@@ -0,0 +1,181 @@
+#include <ntddk.h>
+#include <wdf.h>
+#include <wdm.h>
+#include "MSR.h"
+#include "VMX.h"
+#include "Common.h"
+
+#define ALIGNMENT_PAGE_SIZE   4096
+#define MAXIMUM_ADDRESS	0xffffffffffffffff
+#define VMCS_SIZE   4096
+#define VMXON_SIZE   4096
+
+UINT64 VirtualAddress_to_PhysicalAddress(void* va)
+{
+	return MmGetPhysicalAddress(va).QuadPart;
+}
+
+UINT64 PhysicalAddress_to_VirtualAddress(UINT64 pa)
+{
+	PHYSICAL_ADDRESS PhysicalAddr;
+	PhysicalAddr.QuadPart = pa;
+
+	return MmGetVirtualForPhysical(PhysicalAddr);
+}
+
+
+BOOLEAN Allocate_VMXON_Region(IN PVirtualMachineState vmState)
+{
+	// at IRQL > DISPATCH_LEVEL memory allocation routines don't work
+	if (KeGetCurrentIrql() > DISPATCH_LEVEL)
+		KeRaiseIrqlToDpcLevel();
+
+
+	PHYSICAL_ADDRESS PhysicalMax = { 0 };
+	PhysicalMax.QuadPart = MAXULONG64;
+
+
+	int VMXONSize = 2 * VMXON_SIZE;
+	BYTE* Buffer = MmAllocateContiguousMemory(VMXONSize + ALIGNMENT_PAGE_SIZE, PhysicalMax);  // Allocating a 4-KByte Contigous Memory region
+
+	PHYSICAL_ADDRESS Highest = { 0 }, Lowest = { 0 };
+	Highest.QuadPart = ~0;
+
+	//BYTE* Buffer = MmAllocateContiguousMemorySpecifyCache(VMXONSize + ALIGNMENT_PAGE_SIZE, Lowest, Highest, Lowest, MmNonCached);
+
+	if (Buffer == NULL) {
+		DbgPrint("[*] Error : Couldn't Allocate Buffer for VMXON Region.\n");
+		return FALSE;// ntStatus = STATUS_INSUFFICIENT_RESOURCES;
+	}
+	UINT64 PhysicalBuffer = VirtualAddress_to_PhysicalAddress(Buffer);
+
+	// zero-out memory 
+	RtlSecureZeroMemory(Buffer, VMXONSize + ALIGNMENT_PAGE_SIZE);
+	UINT64 alignedPhysicalBuffer = (BYTE*)((ULONG_PTR)(PhysicalBuffer + ALIGNMENT_PAGE_SIZE - 1) &~(ALIGNMENT_PAGE_SIZE - 1));
+
+	UINT64 alignedVirtualBuffer = (BYTE*)((ULONG_PTR)(Buffer + ALIGNMENT_PAGE_SIZE - 1) &~(ALIGNMENT_PAGE_SIZE - 1));
+
+	DbgPrint("[*] Virtual allocated buffer for VMXON at %llx\n", Buffer);
+	DbgPrint("[*] Virtual aligned allocated buffer for VMXON at %llx\n", alignedVirtualBuffer);
+	DbgPrint("[*] Aligned physical buffer allocated for VMXON at %llx\n", alignedPhysicalBuffer);
+
+	// get IA32_VMX_BASIC_MSR RevisionId
+
+	IA32_VMX_BASIC_MSR basic = { 0 };
+
+
+	basic.All = __readmsr(MSR_IA32_VMX_BASIC);
+
+	DbgPrint("[*] MSR_IA32_VMX_BASIC (MSR 0x480) Revision Identifier %llx\n", basic.Fields.RevisionIdentifier);
+
+	//Changing Revision Identifier
+	*(UINT64 *)alignedVirtualBuffer = basic.Fields.RevisionIdentifier;
+
+
+	int status = __vmx_on(&alignedPhysicalBuffer);
+	if (status)
+	{
+		DbgPrint("[*] VMXON failed with status %d\n", status);
+		return FALSE;
+	}
+
+	vmState->VMXON_REGION = alignedPhysicalBuffer;
+
+	return TRUE;
+}
+
+BOOLEAN Allocate_VMCS_Region(IN PVirtualMachineState vmState)
+{
+	// at IRQL > DISPATCH_LEVEL memory allocation routines don't work
+	if (KeGetCurrentIrql() > DISPATCH_LEVEL)
+		KeRaiseIrqlToDpcLevel();
+
+
+	PHYSICAL_ADDRESS PhysicalMax = { 0 };
+	PhysicalMax.QuadPart = MAXULONG64;
+
+
+	int VMCSSize = 2 * VMCS_SIZE;
+	BYTE* Buffer = MmAllocateContiguousMemory(VMCSSize + ALIGNMENT_PAGE_SIZE, PhysicalMax);  // Allocating a 4-KByte Contigous Memory region
+
+	PHYSICAL_ADDRESS Highest = { 0 }, Lowest = { 0 };
+	Highest.QuadPart = ~0;
+
+	//BYTE* Buffer = MmAllocateContiguousMemorySpecifyCache(VMXONSize + ALIGNMENT_PAGE_SIZE, Lowest, Highest, Lowest, MmNonCached);
+
+	UINT64 PhysicalBuffer = VirtualAddress_to_PhysicalAddress(Buffer);
+	if (Buffer == NULL) {
+		DbgPrint("[*] Error : Couldn't Allocate Buffer for VMCS Region.\n");
+		return FALSE;// ntStatus = STATUS_INSUFFICIENT_RESOURCES;
+	}
+	// zero-out memory 
+	RtlSecureZeroMemory(Buffer, VMCSSize + ALIGNMENT_PAGE_SIZE);
+	UINT64 alignedPhysicalBuffer = (BYTE*)((ULONG_PTR)(PhysicalBuffer + ALIGNMENT_PAGE_SIZE - 1) &~(ALIGNMENT_PAGE_SIZE - 1));
+
+	UINT64 alignedVirtualBuffer = (BYTE*)((ULONG_PTR)(Buffer + ALIGNMENT_PAGE_SIZE - 1) &~(ALIGNMENT_PAGE_SIZE - 1));
+
+
+
+	DbgPrint("[*] Virtual allocated buffer for VMCS at %llx\n", Buffer);
+	DbgPrint("[*] Virtual aligned allocated buffer for VMCS at %llx\n", alignedVirtualBuffer);
+	DbgPrint("[*] Aligned physical buffer allocated for VMCS at %llx\n", alignedPhysicalBuffer);
+
+	// get IA32_VMX_BASIC_MSR RevisionId
+
+	IA32_VMX_BASIC_MSR basic = { 0 };
+
+
+	basic.All = __readmsr(MSR_IA32_VMX_BASIC);
+
+	DbgPrint("[*] MSR_IA32_VMX_BASIC (MSR 0x480) Revision Identifier %llx\n", basic.Fields.RevisionIdentifier);
+
+
+	//Changing Revision Identifier
+	*(UINT64 *)alignedVirtualBuffer = basic.Fields.RevisionIdentifier;
+
+	vmState->VMCS_REGION = alignedPhysicalBuffer;
+
+	return TRUE;
+}
+
+BOOLEAN Allocate_VMM_Stack(int ProcessorID)
+{
+
+	// Allocate stack for the VM Exit Handler.
+	UINT64 VMM_STACK_VA = ExAllocatePoolWithTag(NonPagedPool, VMM_STACK_SIZE, POOLTAG);
+	vmState[ProcessorID].VMM_Stack = VMM_STACK_VA;
+
+	if (vmState[ProcessorID].VMM_Stack == NULL)
+	{
+		DbgPrint("[*] Error in allocating VMM Stack.\n");
+		return FALSE;
+	}
+	RtlZeroMemory(vmState[ProcessorID].VMM_Stack, VMM_STACK_SIZE);
+
+	DbgPrint("[*] VMM Stack for logical processor %d : %llx\n", ProcessorID, vmState[ProcessorID].VMM_Stack);
+
+	return TRUE;
+}
+
+BOOLEAN Allocate_MSR_Bitmap(int ProcessorID)
+{
+
+	// Allocate memory for MSRBitMap
+	vmState[ProcessorID].MSRBitMap = ExAllocatePoolWithTag(NonPagedPool, PAGE_SIZE, POOLTAG);  // should be aligned
+
+	if (vmState[ProcessorID].MSRBitMap == NULL)
+	{
+		DbgPrint("[*] Error in allocating MSRBitMap.\n");
+		return FALSE;
+	}
+	RtlZeroMemory(vmState[ProcessorID].MSRBitMap, PAGE_SIZE);
+
+	vmState[ProcessorID].MSRBitMapPhysical = VirtualAddress_to_PhysicalAddress(vmState[ProcessorID].MSRBitMap);
+
+	DbgPrint("[*] MSR Bitmap address : %llx\n", vmState[ProcessorID].MSRBitMap);
+
+	// For testing purpose :
+	// SetMSRBitmap(0xc0000082, ProcessorID, TRUE, TRUE);
+
+	return TRUE;
+}
