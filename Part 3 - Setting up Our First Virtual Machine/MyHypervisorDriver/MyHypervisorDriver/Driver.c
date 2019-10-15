c14d1fcea3fe7930c18aa6fe54ff7ed2c96ba4d6
Sina Karvandi <SinaKarvandi@users.noreply.github.com>
2019-10-15 22:17:40 +0330

    Update Driver.c

--- Part 3 - Setting up Our First Virtual Machine/MyHypervisorDriver/MyHypervisorDriver/Driver.c
+++ Part 3 - Setting up Our First Virtual Machine/MyHypervisorDriver/MyHypervisorDriver/Driver.c
@@ -46,7 +46,7 @@ VOID DrvUnload(PDRIVER_OBJECT  DriverObject)
 {
 	UNICODE_STRING usDosDeviceName;
 	DbgPrint("[*] DrvUnload Called.");
-	RtlInitUnicodeString(&usDosDeviceName, L"\\Device\\MyHypervisorDevice");
+	RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\MyHypervisorDevice");
 	IoDeleteSymbolicLink(&usDosDeviceName);
 	IoDeleteDevice(DriverObject->DeviceObject);
 }
